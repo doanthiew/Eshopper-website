17e4aaca3ae91c683e558afbccda3469f28a1e40
Dao Cong Tri <daocongtri20031609@gmail.com>
2024-11-28 00:40:57 +0700

    Create branch tri_dev, upload code to new branch tri_dev

---
+++ Eshopper-website/Controllers/BlogController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Eshopper_website.Controllers
+{
+	public class BlogController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
---
+++ Eshopper-website/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Eshopper_website.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
