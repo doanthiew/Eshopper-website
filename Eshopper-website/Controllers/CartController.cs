8d68165bdf182ebc5825c1aff08ada58b4e53a4f
LN <luongquynhnhu0908@gmail.com>
2024-11-28 09:25:48 +0700

    create new branch LN_dev

--- Eshopper-website/Controllers/CartController.cs
+++ Eshopper-website/Controllers/CartController.cs
@@ -8,5 +8,6 @@ namespace Eshopper_website.Controllers
         {
             return View();
         }
+
     }
 }
--- Eshopper-website/Controllers/HomeController.cs
+++ Eshopper-website/Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-using System.Diagnostics;
+    using System.Diagnostics;
 using Eshopper_website.Models;
 using Microsoft.AspNetCore.Mvc;
 
